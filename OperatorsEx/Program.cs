49d038eb6d122eeb5bc4ed0e1df69f8883f21a95
BK <brian_j_king@me.com>
2020-08-24 13:27:21 -0500

    partTwo BJK2420

--- OperatorsEx/Program.cs
+++ OperatorsEx/Program.cs
@@ -31,6 +31,12 @@ namespace OperatorsEx
             return num1 % num2;
         }
 
+        public static double AreaOfCirlce(double r)
+        {
+            return Math.PI * (r * r);
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine($"Add: {add(1, 5)}");
@@ -38,6 +44,20 @@ namespace OperatorsEx
             Console.WriteLine($"Multiply: {multiply(3, 3)}");
             Console.WriteLine($"Divide: {divide(6, 2)}");
             Console.WriteLine($"Remainder: {mod(6, 2)}");
+            //17/4 is 4 remainder 1	
+            var x = 17;
+            var y = 4;
+
+            Console.WriteLine($"{x}/{y} is {y} remainder {mod(x,y)}");
+
+            Console.WriteLine("------------------------- Part 2 -------------------------");
+           
+            Console.WriteLine("What is the Radius of your circle?");
+            
+            var r = double.Parse(Console.ReadLine());
+            var area = AreaOfCirlce(r);
+            Console.WriteLine($"The area of a cirlce with a radius of {r} is {area}");
+
         }
     }
 }
