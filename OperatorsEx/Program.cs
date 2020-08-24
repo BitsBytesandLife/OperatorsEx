76a9248760961f9aadf02639a87688a18816823d
BK <brian_j_king@me.com>
2020-08-24 12:29:41 -0500

    First Commit BJK2420

---
+++ OperatorsEx/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace OperatorsEx
+{
+    class Program
+    {
+        public static int add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+
+        public static int divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+
+        public static int mod(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Add: {add(1, 5)}");
+            Console.WriteLine($"Sub: {sub(5, 4)}");
+            Console.WriteLine($"Multiply: {multiply(3, 3)}");
+            Console.WriteLine($"Divide: {divide(6, 2)}");
+            Console.WriteLine($"Remainder: {mod(6, 2)}");
+        }
+    }
+}
+
